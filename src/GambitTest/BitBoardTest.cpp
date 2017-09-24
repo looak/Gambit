7d3bace062128b99a71054d79e0a76e15a1b1d45
FAITH\Alexander <alexander.loodin.ek@gmail.com>
2017-09-24 22:10:41 +0200

    Rook & King attack tests

--- src/GambitTest/BitBoardTest.cpp
+++ src/GambitTest/BitBoardTest.cpp
@@ -42,5 +42,48 @@ namespace GambitTest
 			Assert::AreEqual(~universe, result ^ attked, L"Something is wrong with the result of attacked squares", LINE_INFO());
 
 		}
+
+		TEST_METHOD(BitBoardTest_KingAttack)
+		{
+			GambitEngine::Board board;
+			Assert::AreEqual(true, board.PlacePiece(WHITE, KING, 'e', 1), L"Failed to place Knight", LINE_INFO());
+			uint64 attked = ~universe;
+			// d1 should be attacked
+			attked |= 1i64 << 3i64;
+			// f1 should be attacked
+			attked |= 1i64 << 5i64;
+			// d2 should be attacked
+			attked |= 1i64 << 11i64;
+			// e2 should be attacked
+			attked |= 1i64 << 12i64;
+			// f2 should be attacked
+			attked |= 1i64 << 13i64;			
+			
+			auto result = board.bitboard.Attacked(WHITE);
+			Assert::AreEqual(~universe, result ^ attked, L"Something is wrong with the result of attacked squares", LINE_INFO());
+		}
+
+		TEST_METHOD(BitBoardTest_Rook)
+		{
+			GambitEngine::Board board;
+			Assert::AreEqual(true, board.PlacePiece(WHITE, ROOK, 'h', 8), L"Failed to place Knight", LINE_INFO());
+			uint64 attked = ~universe;
+			
+			// h file
+			__int64 h7 = 55;
+			for (__int64 hfile = h7; hfile >= 0; hfile-=8)
+			{
+				attked |= 1i64 << hfile;
+			}			
+			// 8 rank
+			uint64 g8 = 62;
+			for (uint64 rank = g8; rank > h7; rank -- )
+			{
+				attked |= 1i64 << rank;
+			}
+
+			auto result = board.bitboard.Attacked(WHITE);
+			Assert::AreEqual(~universe, result ^ attked, L"Something is wrong with the result of attacked squares", LINE_INFO());
+		}
 	};
 };
